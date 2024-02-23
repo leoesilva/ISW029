2bd0a8e3993cb7c26ae126600e3b3491b3f6247d
Leonardo Silva <42047532+leoesilva@users.noreply.github.com>
2024-02-23 10:33:57 -0300

    Aula 02 - Funções

---
+++ aula02/app.js
@@ -0,0 +1,11 @@
+const adicao = require("./adicao")
+const subtracao = require("./subtracao")
+const multiplicacao = require("./multiplicacao")
+const divisao = require("./divisao")
+
+let n1 = 10, n2 = 2;
+
+console.log(adicao(n1, n2))
+console.log(subtracao(n1, n2))
+console.log(multiplicacao(n1, n2))
+console.log(divisao(n1, n2))
---
+++ aula02/divisao.js
@@ -0,0 +1,5 @@
+let divisao = function(n1, n2) {
+    return n1 / n2;
+}
+
+module.exports = divisao;
